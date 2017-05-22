63b691ece82cafbd976c69bfdc0b195f6bbbd527
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-23 02:11:52 +0300

    Added Window Class

---
+++ Decorator/Decorator/Window.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class Window : ModulerWindow
+    {
+        public override string Build()
+        {
+            return "Oluşturuldu";
+        }
+    }
+}
