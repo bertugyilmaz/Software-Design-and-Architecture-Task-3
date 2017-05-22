ef85c5de9939bc59ef782d4ae6698696db6e0a93
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-23 02:30:20 +0300

    Added BackgroundImage Class

---
+++ Decorator/Decorator/BackgroundImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class BackgroundImage : ModulerWindow
+    {
+        ModulerWindow mWindow;
+        public BackgroundImage(ModulerWindow backgroundImage)
+        {
+            mWindow = backgroundImage;
+        }
+
+        public override string Build()
+        {
+            return "Changed Background Image";
+        }
+    }
+}
