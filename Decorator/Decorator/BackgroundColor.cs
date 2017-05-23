fb11e0bd2c26e5940ef036d2477cd6ec286f360c
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-23 10:14:10 +0300

    Added BackgroundColor & WindowShadow Class

---
+++ Decorator/Decorator/BackgroundColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class BackgroundColor : ModulerWindow
+    {
+        ModulerWindow mwindow;
+        public BackgroundColor(ModulerWindow backgroundColor)
+        {
+            mwindow = backgroundColor;
+        }
+        public override string Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Decorator/Decorator/WindowShadow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class WindowShadow : ModulerWindow
+    {
+
+        ModulerWindow mWindonw;
+
+        public WindowShadow(ModulerWindow windowShadow)
+        {
+            mWindonw = windowShadow;
+        }
+
+        public override string Build()
+        {
+            return "Gölgelendirme eklendi";
+        }
+    }
+}
