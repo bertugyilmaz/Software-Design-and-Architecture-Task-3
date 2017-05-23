21036ae76f6793889c429e686f8477ab85a828f6
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-23 12:17:34 +0300

    Added HorizantalScrollBar Class

---
+++ Decorator/Decorator/HorizantalScrollBar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class HorizantalScrollBar : ModulerWindow
+    {
+        ModulerWindow mWindow;
+
+        public HorizantalScrollBar(ModulerWindow horizantalScrollBar)
+        {
+            mWindow = horizantalScrollBar;
+        }
+
+        public override string Build()
+        {
+            return "Yatay Scrool Bar Eklendi";
+        }
+    }
+}
